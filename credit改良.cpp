b5f5a82302b8d5ecbd125a66768f269deeba4fed
25019林后辰 <43640545+mqlingithub@users.noreply.github.com>
2018-11-11 20:52:23 +0800

    Update credit改良.cpp

--- credit改良.cpp
+++ credit改良.cpp
@@ -1,67 +1,39 @@
 #include <iostream>
 using namespace std;
-void check(int);
-void divide(int);
 int main() {
   long int cardnumber;
   cout << "請輸入信用卡號:\n";
   cin >> cardnumber;
-  divide(cardnumber);
-  int sixteenth = cardnumber;
-  divide(cardnumber);
-  int fifteenth = cardnumber;
-  divide(cardnumber);
-  int fourteenth = cardnumber;
-  divide(cardnumber);
-  int thirteenth = cardnumber;
-  divide(cardnumber);
-  int twelveth = cardnumber;
-  divide(cardnumber);
-  int eleventh = cardnumber;
-  divide(cardnumber);
-  int tenth = cardnumber;
-  divide(cardnumber);
-  int ninth = cardnumber;
-  divide(cardnumber);
-  int eighth = cardnumber;
-  divide(cardnumber);
-  int seventh = cardnumber;
-  divide(cardnumber);
-  int sixth = cardnumber;
-  divide(cardnumber);
-  int fifth = cardnumber;
-  divide(cardnumber);
-  int fourth = cardnumber;
-  divide(cardnumber);
-  int third = cardnumber;
-  int firsttwo = cardnumber;
-  divide(cardnumber);
-  int second = cardnumber;
-  divide(cardnumber);
-  int first = cardnumber;
-  int first1 = cardnumber;
-  check(first);
-  check(third);
-  check(fifth);
-  check(seventh);
-  check(ninth);
-  check(eleventh);
-  check(thirteenth);
-  check(fifteenth);
-  int total = first + second  + third + fourth + fifth + sixth + seventh + eighth + ninth + tenth + eleventh + twelveth + thirteenth + fourteenth + fifteenth + sixteenth;
+  long int card1 = cardnumber, card2 = cardnumber / 10, total = 0,checknumber;
+  for ( int a = 0 ; a < 8; a++ ) {
+    int number = card1 % 10;
+    card1 = card1 / 100;
+    total = total + number;
+  }
+  for ( int a = 0 ; a < 8 ; a++ ) {
+    int number = card2 % 10;
+    card2 = card2 / 100;
+    if ( number > 4 ) {
+      number = number * 2 / 10 + number * 2 % 10;
+    }
+    else {
+      number = number * 2;
+    }
+    total = total + number;
+  }
+  for ( int a = 0 ; a < 15 ; a++ ) {
+    cardnumber = cardnumber / 10;
+  }
+  if (total % 10 == 0) {
+    checknumber = 0;
+  }
+  else {
+    checknumber = 10 - total % 10;
+  }
   if ( total % 10 == 0 ) {
-    if ( first1 == 4 ) cout << "這是Visa card。";
-    else if ( first1 == 5 && firsttwo <= 55 && firsttwo >= 51 ) cout << "這是Master card。";
+    if ( cardnumber == 4 ) cout << "這是Visa card。";
+    else if ( cardnumber == 5 ) cout << "這是Master card。";
     else cout << "這是其他信用卡。";
   }
   else cout << "這是其他信用卡。\n";
-}
-void check(int number) {
-  if ( number * 2 >= 10 ) {
-    number = number / 10 + number % 10;
-  }
-  else number = number * 2;
-}
-void divide(int number) {
-  number = number % 10;
 }
